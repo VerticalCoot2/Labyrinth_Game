05035a22f977cf80f7bd19065b5c361381f4e71e
verticalcoot2 <verticalcoot2@gmail.com>
2024-05-15 18:23:14 +0200

    cOMMIT

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    [SerializeField] private int collectibles = 0;
+    [SerializeField] private GameObject colHolder;
+    int maxCol;
+
+    [SerializeField] private GameObject winnerRoomDoor;
+    void Start()
+    {
+        maxCol = colHolder.transform.childCount;
+        winnerRoomDoor.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(collectibles == maxCol)
+        {
+            winnerRoomDoor.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        switch(col.gameObject.tag)
+        {
+            case "collectible":
+                collectibles++;
+                Destroy(col.gameObject);
+                break;
+        }
+    }
+}
